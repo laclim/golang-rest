e065f72d2b85458788f649ca8e222e308205c0b5
laclim <lim.ac96@gmail.com>
2020-05-31 16:57:06 +0800

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/laclim/golang-rest
+
+go 1.14
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
-	
+	fmt.Println("Hello, worasdald.")
 }
